8392027128540a185d830d14e5ecd929096c9c26
taliberlin <118308290+taliberlin@users.noreply.github.com>
2023-06-22 14:56:54 -0700

    added basic structure to Projects

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,47 @@ export default function Projects() {
   return (
     <div className="Projects">
       <h3>Projects</h3>
+      <div className="d-flex justify-content-evenly">
+        <div className="p-1">
+          <img
+            src={require("./weather-app.png")}
+            alt="Weather app"
+            className="img-fluid"
+            width={300}
+          />
+          <p className="text-center">HTML, CSS, JavaScript, React.js</p>
+          <p className="text-center">
+            Open app in{" "}
+            <a href="https://meek-cannoli-bc5bd6.netlify.app/">new window</a>
+          </p>
+        </div>
+        <div className="p-1">
+          <img
+            src={require("./weather-app.png")}
+            alt="Weather app"
+            className="img-fluid"
+            width={300}
+          />
+          <p className="text-center">HTML, CSS, JavaScript, React.js</p>
+          <p className="text-center">
+            Open app in{" "}
+            <a href="https://meek-cannoli-bc5bd6.netlify.app/">new window</a>
+          </p>
+        </div>
+        <div className="p-1">
+          <img
+            src={require("./weather-app.png")}
+            alt="Weather app"
+            className="img-fluid"
+            width={300}
+          />
+          <p className="text-center">HTML, CSS, JavaScript, React.js</p>
+          <p className="text-center">
+            Open app in{" "}
+            <a href="https://meek-cannoli-bc5bd6.netlify.app/">new window</a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
